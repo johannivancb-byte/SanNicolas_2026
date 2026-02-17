c4936ef7123a9e28ffb83e64c9f665b8c07d2255
johannivancb-byte <johannivancb@gmail.com>
2026-02-16 22:18:47 -0500

    Add files via upload

---
+++ reuniones-atencion-data.js
@@ -0,0 +1,330 @@
+// Datos de Reuniones de Área y Atención a Acudientes
+// IE Nuevo Compartir - San Nicolás
+// Generado: 2026
+
+const REUNIONES_AREA = [
+  {
+    "area": "Sociales",
+    "docentes": [
+      "Jorge Sanabria",
+      "Lina Díaz"
+    ],
+    "dia": "Martes",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Religión",
+    "docentes": [
+      "Clara Cardenas",
+      "Leonardo Delgadillo"
+    ],
+    "dia": "Martes",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Lengua Castellana",
+    "docentes": [
+      "Martha Casas",
+      "Margarita Varela",
+      "Nelson Amortegui"
+    ],
+    "dia": "Lunes",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Ciencias Naturales",
+    "docentes": [
+      "Janneth Mancipe",
+      "Wilson Salazar",
+      "Sebastian Guayara",
+      "Fabio Vasquez"
+    ],
+    "dia": "Viernes",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Matemáticas",
+    "docentes": [
+      "Brandon Ortiz",
+      "Olga Rodriguez",
+      "Alejandro Pérez"
+    ],
+    "dia": "Jueves",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Informática y Tecnología",
+    "docentes": [
+      "Carlos Molina",
+      "Edgar Muñoz"
+    ],
+    "dia": "Jueves",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "area": "Inglés",
+    "docentes": [
+      "Pilar Pelaez",
+      "Johana Ricaurte",
+      "Maritza Valderrama"
+    ],
+    "dia": "Lunes",
+    "hora": 6,
+    "horaInicio": "11:10",
+    "horaFin": "12:00",
+    "lugar": "Sala de profesores"
+  }
+];
+
+const ATENCION_ACUDIENTES = [
+  {
+    "docente": "Alejandro Pérez",
+    "dia": "Lunes",
+    "hora": 2,
+    "horario": "6:55 - 7:50",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Brandon Ortiz",
+    "dia": "Lunes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Carlos Alcala",
+    "dia": "Lunes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Carlos Molina",
+    "dia": "Viernes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Clara Cardenas",
+    "dia": "Miércoles",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Edgar Muñoz",
+    "dia": "Miércoles",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Esteban Bejarano",
+    "dia": "Lunes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Fabio Vasquez",
+    "dia": "Martes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Janneth Mancipe",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Johan Suarez",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Johana Ricaurte",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Jorge Sanabria",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Leonardo Delgadillo",
+    "dia": "Martes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Lina Díaz",
+    "dia": "Martes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Luis Gomez",
+    "dia": "Jueves",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Margarita Varela",
+    "dia": "Martes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Maritza Valderrama",
+    "dia": "Martes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Martha Casas",
+    "dia": "Martes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Nelson Amortegui",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Olga Rodriguez",
+    "dia": "Lunes",
+    "hora": 2,
+    "horario": "6:55 - 7:50",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Pilar Pelaez",
+    "dia": "Miércoles",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Sebastian Guayara",
+    "dia": "Lunes",
+    "hora": 3,
+    "horario": "7:50 - 8:45",
+    "lugar": "Sala de profesores"
+  },
+  {
+    "docente": "Wilson Salazar",
+    "dia": "Lunes",
+    "hora": 1,
+    "horario": "6:00 - 6:55",
+    "lugar": "Sala de profesores"
+  }
+];
+
+const SALONES_BASE = {
+  "1005": {
+    "salon_base": "2",
+    "director_grupo": "Lina Díaz"
+  },
+  "1006": {
+    "salon_base": "12",
+    "director_grupo": "Nelson Amortegui"
+  },
+  "1105": {
+    "salon_base": "14",
+    "director_grupo": "Fabio Vasquez"
+  },
+  "1106": {
+    "salon_base": "20",
+    "director_grupo": "Wilson Salazar"
+  },
+  "605": {
+    "salon_base": "3",
+    "director_grupo": "Pilar Pelaez"
+  },
+  "606": {
+    "salon_base": "1",
+    "director_grupo": "Carlos Alcala"
+  },
+  "607": {
+    "salon_base": "15",
+    "director_grupo": "Johana Ricaurte"
+  },
+  "608": {
+    "salon_base": "16",
+    "director_grupo": "Sebastian Guayara"
+  },
+  "705": {
+    "salon_base": "10",
+    "director_grupo": "Clara Cardenas"
+  },
+  "706": {
+    "salon_base": "13",
+    "director_grupo": "Jorge Sanabria"
+  },
+  "707": {
+    "salon_base": "7",
+    "director_grupo": "Brandon Ortiz"
+  },
+  "805": {
+    "salon_base": "18",
+    "director_grupo": "Carlos Molina"
+  },
+  "806": {
+    "salon_base": "11",
+    "director_grupo": "Johan Suarez"
+  },
+  "807": {
+    "salon_base": "4",
+    "director_grupo": "Leonardo Delgadillo"
+  },
+  "905": {
+    "salon_base": "8",
+    "director_grupo": "Maritza Valderrama"
+  },
+  "906": {
+    "salon_base": "9",
+    "director_grupo": "Janneth Mancipe"
+  },
+  "907": {
+    "salon_base": "17",
+    "director_grupo": "Martha Casas"
+  }
+};
